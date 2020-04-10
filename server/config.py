7e36c6c45305e6b0bdeb37718b88e3ccea7d4518
amir mazinani <info.mazinani@gmail.com>
2020-04-10 17:32:12 +0430

    change config file structure

---
+++ server/config.py
@@ -0,0 +1,17 @@
+import json
+
+with open("./config.json") as cg:
+    config = json.load(cg)
+
+class Base():
+    SQLALCHEMY_DATABASE_URI = config["SQLALCHEMY_DATABASE_URI"]
+    SQLALCHEMY_TRACK_MODIFICATIONS = config["SQLALCHEMY_TRACK_MODIFICATIONS"]
+    SECRET_KEY = config["SECRET_KEY"]
+
+
+class Development(Base):
+    pass
+
+
+class Production(Base):
+    pass
