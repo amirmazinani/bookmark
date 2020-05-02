a0b084a3d195d44bfd669311b3c1fc4262dd3502
amir mazinani <info.mazinani@gmail.com>
2020-05-03 01:05:55 +0430

    change link db model structure

--- server/application/components/links/model.py
+++ server/application/components/links/model.py
@@ -13,8 +13,8 @@ class Link(db.Model):
     link = db.Column(db.Text, nullable=False)
     deleted = db.Column(db.Boolean, default=False)
     create_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    update_time = db.Column(db.DateTime, default=0)
-    visit_count = db.Column(db.Integer, default=0, nullable=False)
+    update_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    visit_count = db.Column(db.Integer, default=0)
     # user_id is foreign key
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
 
